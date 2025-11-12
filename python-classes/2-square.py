82a99cf734e488b35422d64a580aa87ea1af6993
garymurasira <g.murasira@alustudent.com>
2025-11-12 12:41:23 +0200

    initial commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,9 +4,9 @@
 
 class Square:
     """documented"""
-    def __init__(self,size=0):
+    def __init__(self, size=0):
         self.__size = size
-        if isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
