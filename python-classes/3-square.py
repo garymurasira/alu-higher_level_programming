8936c53f68761be14162386d8c67b2bb21025e25
garymurasira <g.murasira@alustudent.com>
2025-11-12 12:42:33 +0200

    initial commit

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""documented"""
+
+
+class Square:
+    """documented"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+          
+    def area(self):
+        return self.size * self.size
+      
+square = square()
+square.area()
