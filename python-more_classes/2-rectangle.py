b1e790fb89b2193ece52c8d7d43393e65445efa2
garymurasira <g.murasira@alustudent.com>
2025-11-12 21:28:00 +0200

    initial commit

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -31,3 +31,12 @@ class Rectangle:
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
+
+    def area(self):
+        return self.__width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+          return 0
+        else:
+          return 2 * (self.width + self.height)
