0e9d6c61d3a80d45d9e6d9c292bf4708041b131a
garymurasira <g.murasira@alustudent.com>
2025-11-13 15:47:27 +0200

    initial commit

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,12 +5,9 @@ This module defines MyList, a subclass of list with a custom print method.
 
 
 class MyList(list):
-    """
-    A subclass of list that adds a method to print the list sorted.
-    """
+    """A list subclass with additional functionality for
+    printing sorted contents."""
+
     def print_sorted(self):
-        """
-        Prints the list in ascending sorted order without
-        modifying the original list.
-        """
+        """Print the list elements in ascending sorted order."""
         print(sorted(self))
